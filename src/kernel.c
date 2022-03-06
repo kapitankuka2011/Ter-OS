7d14d23017e6dc2387cf757d99d75ee69e49150b
kapitankuka2011 <kuba.grabus@onet.pl>
2022-03-06 20:43:47 +0100

    beta

---
+++ src/kernel.c
@@ -0,0 +1,116 @@
+#include "kernel.h"
+#include "console.h"
+#include "string.h"
+#include "gdt.h"
+#include "idt.h"
+#include "keyboard.h"
+#include "io_ports.h"
+
+#define BRAND_QEMU  1
+#define BRAND_VBOX  2
+
+
+void __cpuid(uint32 type, uint32 *eax, uint32 *ebx, uint32 *ecx, uint32 *edx) {
+    asm volatile("cpuid"
+                : "=a"(*eax), "=b"(*ebx), "=c"(*ecx), "=d"(*edx)
+                : "0"(type)); // put the type into eax
+}
+
+int cpuid_info(int print) {
+    uint32 brand[12];
+    uint32 eax, ebx, ecx, edx;
+    uint32 type;
+
+    memset(brand, 0, sizeof(brand));
+    __cpuid(0x80000002, (uint32 *)brand+0x0, (uint32 *)brand+0x1, (uint32 *)brand+0x2, (uint32 *)brand+0x3);
+    __cpuid(0x80000003, (uint32 *)brand+0x4, (uint32 *)brand+0x5, (uint32 *)brand+0x6, (uint32 *)brand+0x7);
+    __cpuid(0x80000004, (uint32 *)brand+0x8, (uint32 *)brand+0x9, (uint32 *)brand+0xa, (uint32 *)brand+0xb);
+
+    if (print) {
+        printf("Brand: %s\n", brand);
+        for(type = 0; type < 4; type++) {
+            __cpuid(type, &eax, &ebx, &ecx, &edx);
+            printf("type:0x%x, eax:0x%x, ebx:0x%x, ecx:0x%x, edx:0x%x\n", type, eax, ebx, ecx, edx);
+        }
+    }
+
+    if (strstr(brand, "QEMU") != NULL)
+        return BRAND_QEMU;
+
+    return BRAND_VBOX;
+}
+
+BOOL is_echo(char *b) {
+    if((b[0]=='e')&&(b[1]=='c')&&(b[2]=='h')&&(b[3]=='o'))
+        if(b[4]==' '||b[4]=='\0')
+            return TRUE;
+    return FALSE;
+}
+
+void shutdown() {
+    int brand = cpuid_info(0);
+    // QEMU
+    if (brand == BRAND_QEMU)
+        outports(0x604, 0x2000);
+    else
+        // VirtualBox
+        outports(0x4004, 0x3400);
+}
+
+void kmain() {
+    char buffer[255];
+    const char *shell = "TerOS@terminal>";
+    const char *ver = "Beta";
+
+    gdt_init();
+    idt_init();
+
+    console_init(COLOR_WHITE, COLOR_BLACK);
+
+    keyboard_init();
+
+    printf("Starting OS...\n");
+
+    int c, d;
+   
+    for (c = 1; c <= 20000; c++)
+       for (d = 1; d <= 20000; d++)
+       {}
+
+    printf("Started!\n");
+
+    for (c = 1; c <= 10000; c++)
+       for (d = 1; d <= 10000; d++)
+       {}
+
+    console_clear(COLOR_WHITE, COLOR_BLACK);
+    printf("TerOS %s\n", ver);
+    while(1) {
+        printf(shell);
+        memset(buffer, 0, sizeof(buffer));
+        getstr_bound(buffer, strlen(shell));
+        if (strlen(buffer) == 0)
+            continue;
+        if(strcmp(buffer, "cpuinfo") == 0) {
+            cpuid_info(1);
+        } else if(strcmp(buffer, "help") == 0) {
+            printf("MSISC-TerOS\n");
+            printf("Commands: help, clear/cls, cpuinfo, echo, shutdown, creators, info\n");
+        } else if(is_echo(buffer)) {
+            printf("%s\n", buffer + 5);
+        } else if(strcmp(buffer, "shutdown") == 0) {
+            shutdown();
+        } else if(strcmp(buffer, "creators") == 0) {
+            printf("Project Owners: MSISC Prod.\nProgrammer: Kuba\nInspiration: installing linux\n");
+        } else if(strcmp(buffer, "info") == 0) {
+            printf("\nOS-Info:\nProgramming language: The C Programming Language\nCreation date: 3.6.22\nBootLoader: GNU GRUB\n\n");
+        } else if(strcmp(buffer, "clear") == 0 || strcmp(buffer, "cls") == 0) {
+            console_clear(COLOR_WHITE, COLOR_BLACK);
+        } else {
+            printf("failed to execute (NOT_FOUND): %s\n", buffer);
+        }
+    }
+}
+
+
+
